6877b53a262b4ff7d803d64d561e1fab7ace43fc
termanty <termanty@gmail.com>
2016-05-10 20:26:34 +0300

    ready and working

--- satellite_network.py
+++ satellite_network.py
@@ -11,16 +11,13 @@ from numpy.linalg import norm
 import math
 import urllib.request
 
-
-earth = 6371
-
+R = 6371
 
 # Getting data
 url = "https://space-fast-track.herokuapp.com/generate"
 with urllib.request.urlopen(url) as response:
     data = response.read().decode().split()
-
-print(data[1])
+print('SEED: ' + str(data[1]))
 
 # Extracting location data
 satellites = np.array([i.split(',')[1:] for i in data[2:-1]])
@@ -29,11 +26,8 @@ end = np.append(data[-1].split(',')[3:], ['0'])
 location = np.vstack((satellites, start, end))
 location = location.astype(float)
 
-#print(location)
-
-
-# Change alltitude to distance from the earth center
-location[:,2] = location[:,2] + earth
+# Change alltitude to distance from the R center
+location[:,2] = location[:,2] + R
 
 # Degrees to radians
 location[:,:2] = np.radians(location[:,:2])
@@ -45,46 +39,42 @@ y = zplane * np.sin(location[:,1])
 z = location[:,2] * np.sin(location[:,0])
 xyz_location = np.vstack((x,y,z)).T
 
-#print(xyz_location)
-
-
 # Projection of a point to 3D-line
-def p2line(point, linep_a, linep_b):
+def projectpoint2line(point, linep_a, linep_b):
     ab = linep_b - linep_a
     ap = point - linep_a
-    proj = linep_a + np.dot(ap,ab)/np.dot(ab,ab)*ab
-    dis = norm(proj)
-    if isinstance(point, np.ndarray):
-        if norm(linep_b - point) > earth:
-            return False
-    return dis > earth 
+    return linep_a + np.dot(ap,ab)/np.dot(ab,ab)*ab
 
+# Checks if two satellites are able to communicate
+def has_sat2sat_connection(satA, satB):
+    closest2Rcenter = projectpoint2line(0, satA, satB)
+    return norm(closest2Rcenter) > R
+
+# Checks if ground location can connect to satellite
+def hasconnection2sat(ground, sat):
+    satttt = projectpoint2line(sat, 0, ground) 
+    distance = norm(satttt)
+    return distance > R and norm(sat - ground) < R
 
 # Satellite adjency matrix A
 sat = xyz_location[:-2]
 A = np.zeros((20,20), dtype='int')
 for i in range(20):
     for j in range(20):
-        if not(i==j) and p2line(0,sat[i],sat[j]):
+        if not(i==j) and has_sat2sat_connection(sat[i],sat[j]):
             A[i][j] = 1
-
-print(A)
-
 
 # Satellites visible to starting and ending locations
 start = xyz_location[-2]
 end = xyz_location[-1]
-visible_start = [i for i in range(20) if p2line(sat[i],0,start)]
-visible_end = [i for i in range(20) if p2line(sat[i],0,end)]
+visible_start = [i for i in range(20) if hasconnection2sat(start, sat[i])]
+visible_end = [i for i in range(20) if hasconnection2sat(end, sat[i])]
 
-
-# searching shortest route
-print(visible_start)
-print(visible_end)
-
+# Lists all satellites which are able to communicate with this satellite
 def connections(satellite):
     return [i for i in range(20) if A[satellite,i]]
 
+# Breadth first search for path finding
 def search(start, end):
     queue = [(start, [start])]
     while queue:
@@ -95,13 +85,11 @@ def search(start, end):
             else:
                 queue.append((next, path + [next]))
 
+# Finding solution
 if not visible_start or not visible_end:
-    print('no path')
-elif set(visible_start) == set(visible_end):
-    print('common satellite')
-    print(set(visible_start).intersection(visible_end))
+    print('warning! no path excist!')
 else:
-    print('path search')
-    print(list(search(visible_start[0], visible_end)))
-
+    paths = [search(i, visible_end) for i in visible_start]
+    print('shortest path:')
+    print(min(paths, key=len))
 
