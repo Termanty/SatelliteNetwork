cc95756c272a73a797231f206cd01cf42935c8a1
termanty <termanty@gmail.com>
2016-05-11 10:20:09 +0300

    Small changes

--- satellite_network.py
+++ satellite_network.py
@@ -2,13 +2,12 @@
 #
 # Reaktor Orbital Challenge
 #
-# @Tero Mäntylä
+# Tero Mäntylä
 # termanty@cs.helsinki.fi
 #
 
 import numpy as np
 from numpy.linalg import norm
-import math
 import urllib.request
 
 R = 6371
@@ -17,62 +16,61 @@ R = 6371
 url = "https://space-fast-track.herokuapp.com/generate"
 with urllib.request.urlopen(url) as response:
     data = response.read().decode().split()
-print('SEED: ' + str(data[1]))
+print('SEED: ' + data[1])
 
-# Extracting location data
+# Extracting locations data
 satellites = np.array([i.split(',')[1:] for i in data[2:-1]])
 start = np.append(data[-1].split(',')[1:3], ['0'])
 end = np.append(data[-1].split(',')[3:], ['0'])
-location = np.vstack((satellites, start, end))
-location = location.astype(float)
+locations = np.vstack((satellites, start, end))
+locations = locations.astype(float)
 
 # Change alltitude to distance from the R center
-location[:,2] = location[:,2] + R
+locations[:,2] = locations[:,2] + R
 
 # Degrees to radians
-location[:,:2] = np.radians(location[:,:2])
+locations[:,:2] = np.radians(locations[:,:2])
 
 # Change to XYZ-coordinate system
-zplane = location[:,2] * np.cos(location[:,0])
-x = zplane * np.cos(location[:,1])
-y = zplane * np.sin(location[:,1])
-z = location[:,2] * np.sin(location[:,0])
-xyz_location = np.vstack((x,y,z)).T
+zplane = locations[:,2] * np.cos(locations[:,0])
+x = zplane * np.cos(locations[:,1])
+y = zplane * np.sin(locations[:,1])
+z = locations[:,2] * np.sin(locations[:,0])
+xyz_locations = np.vstack((x,y,z)).T
 
-# Projection of a point to 3D-line
-def projectpoint2line(point, linep_a, linep_b):
-    ab = linep_b - linep_a
-    ap = point - linep_a
-    return linep_a + np.dot(ap,ab)/np.dot(ab,ab)*ab
+# Projection of a point to the 3D-line
+def projectpoint2line(p, a, b):
+    ab = b - a
+    ap = p - a
+    return a + np.dot(ap,ab)/np.dot(ab,ab)*ab
 
 # Checks if two satellites are able to communicate
-def has_sat2sat_connection(satA, satB):
+def hassat2satconnection(satA, satB):
     closest2Rcenter = projectpoint2line(0, satA, satB)
     return norm(closest2Rcenter) > R
 
-# Checks if ground location can connect to satellite
+# Checks if ground locations can connect to satellite
 def hasconnection2sat(ground, sat):
-    satttt = projectpoint2line(sat, 0, ground) 
-    distance = norm(satttt)
-    return distance > R and norm(sat - ground) < R
+    proj = projectpoint2line(sat, 0, ground) 
+    return norm(proj) > R and norm(sat - ground) < R
 
 # Satellite adjency matrix A
-sat = xyz_location[:-2]
+sats = xyz_locations[:-2]
 A = np.zeros((20,20), dtype='int')
 for i in range(20):
     for j in range(20):
-        if not(i==j) and has_sat2sat_connection(sat[i],sat[j]):
+        if not(i==j) and hassat2satconnection(sats[i],sats[j]):
             A[i][j] = 1
 
 # Satellites visible to starting and ending locations
-start = xyz_location[-2]
-end = xyz_location[-1]
-visible_start = [i for i in range(20) if hasconnection2sat(start, sat[i])]
-visible_end = [i for i in range(20) if hasconnection2sat(end, sat[i])]
+start = xyz_locations[-2]
+end = xyz_locations[-1]
+visible_start = [i for i in range(20) if hasconnection2sat(start, sats[i])]
+visible_end = [i for i in range(20) if hasconnection2sat(end, sats[i])]
 
 # Lists all satellites which are able to communicate with this satellite
-def connections(satellite):
-    return [i for i in range(20) if A[satellite,i]]
+def connections(sat):
+    return [i for i in range(20) if A[sat,i]]
 
 # Breadth first search for path finding
 def search(start, end):
